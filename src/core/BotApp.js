74092a9e237b8f73a77dc66eec5ca74b42c04834
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2026-09-07 08:42:45 +0100

    refactor: make bot initialization idempotent

--- src/core/BotApp.js
+++ src/core/BotApp.js
@@ -20,42 +20,27 @@ class BotApp {
     this.events = new EventBus();
     this.db = new JsonDatabase({ rootDir, logger: this.logger });
     this.permissions = new PermissionManager(this.config);
-
     this.connection = new ConnectionManager({ config: this.config, state: this.state, events: this.events, logger: this.logger, rootDir });
-    this.commands = new CommandRegistry({
-      commandsDir: path.join(rootDir, 'src', 'cmds'),
-      config: this.config,
-      db: this.db,
-      permissions: this.permissions,
-      logger: this.logger,
-      state: this.state,
-    });
-    this.eventLoader = new EventLoader({
-      eventsDir: path.join(rootDir, 'src', 'events'),
-      bus: this.events,
-      logger: this.logger,
-      apiProvider: () => this.connection.api,
-    });
-
+    this.commands = new CommandRegistry({ commandsDir: path.join(rootDir, 'src', 'cmds'), config: this.config, db: this.db, permissions: this.permissions, logger: this.logger, state: this.state });
+    this.eventLoader = new EventLoader({ eventsDir: path.join(rootDir, 'src', 'events'), bus: this.events, logger: this.logger, apiProvider: () => this.connection.api });
     this.startedAt = Date.now();
     this._shutdownBound = false;
+    this._initialized = false;
   }
 
   async init() {
+    if (this._initialized) return this;
     await this.db.init();
     this.commands.load();
     this.eventLoader.load();
     this.events.on('message', async ({ api, event }) => this.commands.execute(api, event));
     this.events.on('connection:error', error => this.logger.error('Connection error:', error));
     this._bindShutdownSignals();
+    this._initialized = true;
     return this;
   }
 
-  async start() {
-    await this.init();
-    await this.connection.connect();
-    return this;
-  }
+  async start() { await this.init(); await this.connection.connect(); return this; }
 
   async shutdown(signal = 'manual') {
     this.logger.info(`Shutting down (${signal})...`);
@@ -71,9 +56,7 @@ class BotApp {
   _bindShutdownSignals() {
     if (this._shutdownBound) return;
     this._shutdownBound = true;
-    const shutdown = signal => this.shutdown(signal)
-      .then(() => process.exit(0))
-      .catch(error => { this.logger.error('Shutdown failed:', error); process.exit(1); });
+    const shutdown = signal => this.shutdown(signal).then(() => process.exit(0)).catch(error => { this.logger.error('Shutdown failed:', error); process.exit(1); });
     process.once('SIGINT', () => shutdown('SIGINT'));
     process.once('SIGTERM', () => shutdown('SIGTERM'));
   }
