297cfacb9a88e9de0c07bd570dd757ff7de45837
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2026-09-07 08:37:39 +0100

    refactor(core): add command context API

---
+++ src/core/CommandContext.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class CommandContext {
+  constructor({ api, message, args, command, db, config, permissions, logger, registry }) {
+    this.api = api;
+    this.message = message;
+    this.args = args;
+    this.command = command;
+    this.db = db;
+    this.config = config;
+    this.permissions = permissions;
+    this.logger = logger;
+    this.registry = registry;
+    this.user = null;
+    this.group = null;
+  }
+
+  get userID() { return this.message?.senderID; }
+  get threadID() { return this.message?.threadID; }
+  get prefix() { return this.config.get('prefix', '/'); }
+
+  reply(text, ...extra) {
+    return this.api.sendMessage(text, this.threadID, ...extra);
+  }
+
+  send(text, ...extra) { return this.reply(text, ...extra); }
+  react(reaction) { return this.api.setMessageReaction(reaction, this.message.messageID, () => {}); }
+
+  permissionLevel() {
+    return this.permissions.levelFor(this.userID, this.threadID);
+  }
+
+  hasRole(level) {
+    return this.permissions.hasLevel(this.userID, this.threadID, level);
+  }
+
+  isOwner() { return this.permissions.isOwner(this.userID); }
+  isAdmin() { return this.hasRole(2); }
+  isGroupAdmin() { return this.hasRole(1); }
+}
+
+module.exports = CommandContext;
