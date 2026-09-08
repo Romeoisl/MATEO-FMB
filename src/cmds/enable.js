09cdd3fce00d3a73fb533ea1fa6b6aa81b4159e2
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2026-09-08 11:31:30 +0100

    cleanup: remove duplicate enable command

--- src/cmds/enable.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-module.exports={name:'enable',category:'group',description:'Enable MATEO-FMB in this group.',usage:'/enable',role:1,cooldown:3,async execute(ctx){await ctx.groups.set(ctx.threadID,'enabled',true);return ctx.reply(ctx.format('Group',['MATEO-FMB is enabled here.']));}};
