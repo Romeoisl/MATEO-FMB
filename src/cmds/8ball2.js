b6a9ee3b3e5810a356c58482a249c62dc9f4cc05
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2026-09-08 11:30:57 +0100

    cleanup: remove duplicate 8ball command

--- src/cmds/8ball2.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const a=['Yes.','No.','Probably.','Ask again later.','Definitely.','Unlikely.'];
-module.exports={name:'8ball2',category:'fun',description:'Ask a simple yes/no question.',usage:'/8ball2 <question>',role:0,cooldown:3,async execute(ctx){if(!ctx.args.length)return ctx.reply(ctx.error('Ask a question.'));return ctx.reply(ctx.format('8 Ball',[a[Math.floor(Math.random()*a.length)]]));}};
