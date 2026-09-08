9167a5ff87026e20c688af5732d11069a39f5f73
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2026-09-08 11:31:13 +0100

    cleanup: remove duplicate ping command

--- src/cmds/ping2.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-module.exports={name:'ping2',aliases:['pong'],category:'system',description:'Return a detailed runtime ping.',usage:'/ping2',role:0,cooldown:2,async execute(ctx){return ctx.reply(ctx.format('Ping',['Pong.','Process uptime: '+Math.floor(process.uptime())+'s']));}};
